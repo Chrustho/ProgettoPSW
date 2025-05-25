9bc0ef61e45a111e7240c736ba0b7fded77ee736
Chrustho <christianbruni131@gmail.com>
2025-05-25 18:33:01 +0200

    setup keycloak e autenticazione

---
+++ src/main/java/com/example/progettopsw/controllers/rest/ProgettoController.java
@@ -0,0 +1,38 @@
+package com.example.progettopsw.controllers.rest;
+
+import org.springframework.ui.Model;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProgettoController {
+    /**
+     * Maps the root URL ("/") to the home page.
+     *
+     * @return the name of the view to render for the home page
+     */
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    /**
+     * Maps the "/menu" URL to the menu page and sets the authenticated user's username in the model.
+     *
+     * @param user  the authenticated OIDC (OpenID Connect) user
+     * @param model Model object for passing data to the view
+     * @return the name of the view to render for the menu page, or redirects to home if user is not authenticated
+     */
+    @GetMapping("/home")
+    public String home(@AuthenticationPrincipal OidcUser user, Model model) {
+        if (user != null) {
+            model.addAttribute("username", user.getPreferredUsername());
+        } else {
+            return "redirect:/";  // Redirect to home if not authenticated
+        }
+        return "home";
+    }
+}
+
